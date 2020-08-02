07904bbe7f27cd6002e8aba811437e620eeab9e9
Stanley <chido.nduaguibe@gmail.com>
2020-08-02 12:22:15 +0100

    he

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -116,11 +116,191 @@
             This also helps to reduce the crime rate in such communities.
           </div>
           <div class="btn-holder">
-             <a class=" btn btn-bbhf bbhf-orange " href='/donate/{{ Config::get('constants.services_route')[1] }}'>> Donate for this cause </a>
+             <a class=" btn btn-bbhf bbhf-orange " href='/donate/{{ Config::get('constants.services_route')[1] }}'> Donate for this cause </a>
               </div>
            </div>
          </div>
         </div>
+
+
+
+
+       <div class="write-up" id="{{ Config::get('constants.services_route')[2]}}">
+    <div class="section-sub-header">
+ 
+      Competitions and Scholarships
+    </div>
+    <div class="row p-3">
+      <div class="col-md-3">
+      <img src="{{asset('images/child-slippers.jpg')}}"  class='service-image'/>
+      </div>
+      <div class="col-md-9">
+    <div class="main-write-up">
+     We are Blessed to Bless Hands Foundation(BBHF), and we exist to;  through effective partnerships with individuals and corporate organisations meet the health, social, economic, educational, communal and spiritual needs of all people, especially in Africa. 
+ 
+     We believe in the ability of individuals and corporate organisations, to through proactive commitments, birth impactful and productive change in the lives of all people especially in African communities. 
+     
+     At BBHF, we are committed to providing enabling opportunities, platforms and systems for all people, particularly the youths and young people, to lead a self-sustaining and productive life for self, and be able to contribute to the family, Community and society sustainability and development. 
+     
+     
+     We are dedicated to supporting ministers of the gospel, particularly missionaries with aids to help in their works of ministry and we work with local churches to share the goodness of Jesus Christ, calling many to eternal life.
+     
+     
+     We daily nurture, train and build social change makers through our Volunteering programmes and the BBHF School of Social Change, with these individuals going on to be Agents of Change, all over the World, especially in Africa.
+     We have a network of skilled Volunteers and members across the world who drive our vision and bring their very best to the frontiers of attaining the BBHF vision.
+ 
+ Our leadership is made up of an amazing team of passionate and dedicated individuals, who are the best at driving the BBHF vision and always providing  a dynamic, transparent, accountable and innovative leadership.
+    </div>
+    <div class="btn-holder">
+      <a class=" btn btn-bbhf bbhf-green " href='/projects/{{ Config::get('constants.service_routes')[2] }}'> See Opportunities </a>
+      <a class=" btn btn-bbhf bbhf-orange "  href='/donate/{{ Config::get('constants.services_route')[2] }}'> Donate for this cause </a>
+     
+      </div>
+     </div>
+  
+    </div>
+  </div>
+
+
+
+
+  <div class="write-up" id="{{ Config::get('constants.services_route')[3]}}">
+    <div class="section-sub-header"> 
+    Medical Mission Outreach  
+        </div>
+    <div class="row p-3">
+      <div class="col-md-3">
+      <img src="{{asset('images/medical.jpg')}}"  class='service-image'/>
+      </div>
+      <div class="col-md-9">
+    <div class="main-write-up">
+      The Medical mission outreaches of the Blessed to Bless Hands Foundation(BBHF) is a community based free health services targeted at the rural communities. We screen and test for but not limited to the following; Human Immunodeficiency Virus (HIV), Hepatitis B surface   Antigen( HbsAg), Hepatitis C Virus ( HCV), Diabetics Mellitus and Hypertension etc, with Status disclosure and counselling, linkage services to the nearest health centre for individuals needing treatments.
+      Free pharmacological and non- pharmacological treatments are also offered to individual patients with diagnosed illnesses on spot, with free surgical services done as is feasible. These services are rendered by our foundation's pool of health care personnels, Volunteers, associates and funded by our sponsors in collaboration with individuals and/ or corporate organisations.
+    </div>
+    <div class="btn-holder">
+      <a class=" btn btn-bbhf bbhf-orange "  href='/donate/{{ Config::get('constants.services_route')[3] }}'> Donate for this cause </a>
+     
+      </div>
+   </div>
+  </div>
+   </div>
+
+
+
+
+
+   v class="write-up" id="{{ Config::get('constants.services_route')[4]}}">
+    <div class="section-sub-header">
+ 
+      Evangelical Outreaches
+    </div>
+    <div class="row p-3">
+      <div class="col-md-3">
+      <img src="{{asset('images/old-woman-prayer.jpg')}}"  class='service-image'/>
+      </div>
+      <div class="col-md-9">
+    <div class="main-write-up">
+      We express a sincere desire to see souls be saved and many come to eternal life in Jesus Christ name. 
+      Therefore, in liaison with communities and local churches we carry out evangelical outreaches where we preach the goodness of Jesus Christ and calling many to eternal life. 
+      The new converts are linked up with the local churches and pastor(s) for continuity of their Christian journey.</div>
+  
+   <div class="btn-holder">
+    <a class=" btn btn-bbhf bbhf-orange "  href='/donate/{{ Config::get('constants.services_route')[4] }}'> Donate for this cause </a>
+     
+    </div>
+  </div>
+</div>
+   </div>
+
+
+
+
+
+   <div class="write-up" id="{{ Config::get('constants.services_route')[5]}}">
+    <div class="section-sub-header">
+ 
+      Education
+    </div>
+    <div class="row p-3">
+      <div class="col-md-3">
+      <img src="{{asset('images/school.jpg')}}"  class='service-image'/>
+      </div>
+      <div class="col-md-9">
+    <div class="main-write-up">
+     We are Blessed to Bless Hands Foundation(BBHF), and we exist to;  through effective partnerships with individuals and corporate organisations meet the health, social, economic, educational, communal and spiritual needs of all people, especially in Africa. 
+ 
+     We believe in the ability of individuals and corporate organisations, to through proactive commitments, birth impactful and productive change in the lives of all people especially in African communities. 
+     
+     At BBHF, we are committed to providing enabling opportunities, platforms and systems for all people, particularly the youths and young people, to lead a self-sustaining and productive life for self, and be able to contribute to the family, Community and society sustainability and development. 
+     
+     
+     We are dedicated to supporting ministers of the gospel, particularly missionaries with aids to help in their works of ministry and we work with local churches to share the goodness of Jesus Christ, calling many to eternal life.
+     
+     
+     We daily nurture, train and build social change makers through our Volunteering programmes and the BBHF School of Social Change, with these individuals going on to be Agents of Change, all over the World, especially in Africa.
+     We have a network of skilled Volunteers and members across the world who drive our vision and bring their very best to the frontiers of attaining the BBHF vision.
+ 
+ Our leadership is made up of an amazing team of passionate and dedicated individuals, who are the best at driving the BBHF vision and always providing  a dynamic, transparent, accountable and innovative leadership.
+    </div>
+    <div class="btn-holder">
+      <a class=" btn btn-bbhf bbhf-orange "  href='/donate/{{ Config::get('constants.services_route')[5] }}'> Donate for this cause </a>
+   
+      </div>
+    </div>
+   </div>
+
+  </div>
+
+
+
+
+
+
+  <div class="write-up" id="{{ Config::get('constants.services_route')[6]}}">
+    <div class="section-sub-header">
+ 
+      Social Schemes
+    </div>
+    <div class="row p-3">
+      <div class="col-md-3">
+      <img src="{{asset('images/village-people-listen.jpg')}}"  class='service-image'/>
+      </div>
+      <div class="col-md-9">
+    <div class="main-write-up">
+     We are Blessed to Bless Hands Foundation(BBHF), and we exist to;  through effective partnerships with individuals and corporate organisations meet the health, social, economic, educational, communal and spiritual needs of all people, especially in Africa. 
+ 
+     We believe in the ability of individuals and corporate organisations, to through proactive commitments, birth impactful and productive change in the lives of all people especially in African communities. 
+     
+     At BBHF, we are committed to providing enabling opportunities, platforms and systems for all people, particularly the youths and young people, to lead a self-sustaining and productive life for self, and be able to contribute to the family, Community and society sustainability and development. 
+     
+     
+     We are dedicated to supporting ministers of the gospel, particularly missionaries with aids to help in their works of ministry and we work with local churches to share the goodness of Jesus Christ, calling many to eternal life.
+     
+     
+     We daily nurture, train and build social change makers through our Volunteering programmes and the BBHF School of Social Change, with these individuals going on to be Agents of Change, all over the World, especially in Africa.
+     We have a network of skilled Volunteers and members across the world who drive our vision and bring their very best to the frontiers of attaining the BBHF vision.
+ 
+ Our leadership is made up of an amazing team of passionate and dedicated individuals, who are the best at driving the BBHF vision and always providing  a dynamic, transparent, accountable and innovative leadership.
+    </div>
+     <div class="btn-holder">
+      <a class=" btn btn-bbhf bbhf-orange "  href='/donate/{{ Config::get('constants.services_route')[6] }}'> Donate for this cause </a>
+   
+      </div>
+    </div>
+   </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
