cc0aba3aa2d87f8fc1592bd952f030b742c85279
Stanley <chido.nduaguibe@gmail.com>
2020-08-02 11:53:21 +0100

    he

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
         <link rel="stylesheet" href="{{URL::asset('css/dist/css/bootstrap.min.css')}}" >
-        <link rel="stylesheet" href="{{URL::asset('fontawesome/css/all.css')}}" >
+     
 
        <link rel="stylesheet" href="{{URL::asset('css/styles.css')}}" />
        <link rel="stylesheet" href="{{URL::asset('css/header.css')}}" />
