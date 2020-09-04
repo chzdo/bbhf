a2d0f4923d0a84e83f7d355f2cfb7d3298c46a8e
Stanley <chido.nduaguibe@gmail.com>
2020-09-04 11:04:32 +0100

    hello

--- app/Http/Controllers/chat.php
+++ app/Http/Controllers/chat.php
@@ -42,7 +42,7 @@ class chat extends Controller
   {
   $role = Auth::user()->user->role_id;
  
- return $role;
+try{
     if ( $req->group == 1 && ( $role == 1 || $role = 4)){
     $r = VolunteerChat::with('user')->orderByDesc('id')->paginate(15);
     }else if ($req->group == 2 && ( $role == 2 || $role = 4)){
@@ -55,6 +55,9 @@ class chat extends Controller
       return  response()->json(['code' => 0, 'message' => 'invalid access']);
     }
     return  response()->json(['code' => 1, 'message' => $r]);
+  }catch(\Exception $e){
+    return $e;
+  }
   }
 
   /**
